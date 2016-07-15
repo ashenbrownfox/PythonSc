6b70cb33817209ab3749b98f2e24b1a32778e2d4
ashenbrownfox <newhoopsdynasty@gmail.com>
2016-07-15 16:47:18 -0400

    MOdified the ip address getter, created robots.txt getter

--- ip_address.py
+++ ip_address.py
@@ -8,3 +8,6 @@ def get_ip_address(url):
     return result[marker:].splitlines()[0]
 
 print(get_ip_address('thenewboston.com'))
+
+
+#nmap - F 54.186.250.79(ip address), returns what services they are using and other goodies
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 #top level domain ex: github.com
+#robots.txt is a place to put list of files you don't want google to crawl
+
+from general import *
+from domain import *
+
+
+ROOT_DIR = "companies"
